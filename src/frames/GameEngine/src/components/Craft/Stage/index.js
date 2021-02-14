41bb6439981130ae5c3e6c3fa2e94406f93782ed
hello@bennyschmidt.com <hello@bennyschmidt.com>
2021-02-14 13:45:18 -0800

    StagePlayer fundamentals

--- src/frames/GameEngine/src/components/Craft/Stage/index.js
+++ src/frames/GameEngine/src/components/Craft/Stage/index.js
@@ -2,8 +2,6 @@
 let [stage, setStage] = useState();
 let [camera, setCamera] = useState();
 let [renderer, setRenderer] = useState();
-let [mesh, setMesh] = useState();
-let [meshRotation, setMeshRotation] = useState([0, 0, 0]);
 
 /*
   Stage
@@ -13,14 +11,18 @@ class Stage extends FixedCanvas2D {
   constructor(selector) {
     super(selector, 'webgl');
 
-    this.setupRenderer();
-    this.setupThreeScene();
-    this.setupCamera();
-    this.setupMesh();
-  }
+    // Setup renderer
+    renderer = setRenderer(
+      new THREE.WebGLRenderer({ canvas })
+    );
 
-  // Instantiate camera
-  setupCamera() {
+    renderer.setClearColor(0);
+    renderer.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    // Setup stage
+    stage = setStage(new THREE.Scene());
+
+    // Setup camera
     camera = setCamera(
       new THREE.PerspectiveCamera(
         75,
@@ -32,45 +34,9 @@ class Stage extends FixedCanvas2D {
     stage.add(camera);
   }
 
-  // Instantiate renderer
-  setupRenderer() {
-    renderer = setRenderer(
-      new THREE.WebGLRenderer({ canvas })
-    );
-
-    renderer.setClearColor(0);
-    renderer.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-  }
-
-  // Instantiate THREE.Scene
-  setupThreeScene() {
-    stage = setStage(new THREE.Scene());
-  }
-
-  // Create & add the 3D stage object
-  setupMesh() {
-    mesh = setMesh(
-      new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial({ color: COLORS.GREEN })
-      )
-    );
-
-    stage.add(mesh);
-  }
-
   // Handle render
   onRender() {
     renderer.render(stage, camera);
-
-    meshRotation = setMeshRotation([
-      meshRotation[0] + 0.0,
-      meshRotation[1] + 0.01,
-      meshRotation[2] + 0.0
-    ]);
-
-    mesh.rotation.set(...meshRotation);
-    mesh.position.set(0, 0, 3);
 
     if (Player) {
       Player.onRender();
--- src/frames/GameEngine/src/components/Craft/StagePlayer/index.js
+++ src/frames/GameEngine/src/components/Craft/StagePlayer/index.js
@@ -1,3 +1,9 @@
+// State variables
+let [meshRotation, setMeshRotation] = useState([0, 0, 0]);
+let [playerMesh, setPlayerMesh] = useState();
+let [stageZ, setStageZ] = useState(0);
+let [jumpY, setJumpY] = useState(0);
+
 /*
   StagePlayer
 */
@@ -6,11 +12,124 @@ class StagePlayer extends FixedCanvas2DPlayer {
   constructor(selector) {
     super(selector);
 
-    this.element.setAttribute('class', 'StagePlayer');
+    document.body.removeChild(this.element);
+    this.element = renderer.domElement;
+
+    playerMesh = setPlayerMesh(
+      new THREE.Mesh(
+        new THREE.BoxGeometry(.5, 1.5, .1),
+        new THREE.MeshBasicMaterial({
+          color: COLORS.GREEN,
+          opacity: 0
+        })
+      )
+    );
+
+    stage.add(playerMesh);
+  }
+
+  jump() {
+    jumpY = setJumpY(1);
+
+    setTimeout(() => {
+      jumpY = setJumpY(0);
+    }, 200);
+  }
+
+  // Handle keydown
+  onKeyDown(event) {
+    // Early escape if tile map isAnimating or input is invalid
+    if (!event?.code) {
+      return;
+    }
+
+    const keyCode = event.code.toUpperCase().replace('KEY', '');
+
+    // Early escape if jumping, moving, or input is invalid
+    if (jumpY || isMoving || !KEYS[keyCode]) {
+      return;
+    }
+
+    switch (keyCode) {
+      case KEY_BINDINGS.UP:
+        meshRotation = setMeshRotation([0, 0, 0]);
+        stageZ = setStageZ(Math.max(-6, stageZ - .1));
+
+        break;
+
+      case KEY_BINDINGS.LEFT:
+        meshRotation = setMeshRotation([0, 2, 0]);
+        stageX = setStageX(Math.max(-6, stageX - .1));
+
+        break;
+
+      case KEY_BINDINGS.DOWN:
+        meshRotation = setMeshRotation([0, -3, 0]);
+        stageZ = setStageZ(Math.min(3.5, stageZ + .1));
+
+        break;
+
+      case KEY_BINDINGS.RIGHT:
+        meshRotation = setMeshRotation([0, -2, 0]);
+        stageX = setStageX(Math.min(6, stageX + .1));
+
+        break;
+
+      case KEY_BINDINGS.JUMP:
+        this.jump();
+
+        break;
+
+      default:
+        break;
+    }
+
+    isMoving = setIsMoving(true);
+
+    setTimeout(() => {
+      isMoving = setIsMoving(false);
+    }, 17);
   }
 
   // Handle render
   onRender() {
+    let rotationX = 0;
+    let rotationY = 0;
+    let rotationZ = 0;
 
+    if (playerMesh.rotation.x < meshRotation[0]) {
+      rotationX = playerMesh.rotation.x + .1;
+    }
+
+    if (playerMesh.rotation.x > meshRotation[0]) {
+      rotationX = playerMesh.rotation.x - .1;
+    }
+
+    if (playerMesh.rotation.y < meshRotation[1]) {
+      rotationY = playerMesh.rotation.y + .1;
+    }
+
+    if (playerMesh.rotation.y > meshRotation[1]) {
+      rotationY = playerMesh.rotation.y - .1;
+    }
+
+    if (playerMesh.rotation.z < meshRotation[2]) {
+      rotationZ = playerMesh.rotation.z + .1;
+    }
+
+    if (playerMesh.rotation.z > meshRotation[2]) {
+      rotationZ = playerMesh.rotation.z - .1;
+    }
+
+    if (playerMesh.position.y < jumpY) {
+      stageY = setStageY(Math.min(1, stageY + .1));
+    }
+
+    if (playerMesh.position.y > jumpY) {
+      stageY = setStageY(Math.max(0, stageY - .1));
+    }
+
+    playerMesh.rotation.set(rotationX, rotationY, rotationZ);
+    playerMesh.position.set(stageX, stageY, stageZ);
   }
 }
